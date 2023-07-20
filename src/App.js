0fe9518af7e3cd41f51ec4bcab4137c61149ae97
AkalyaRajendran <139786884+AkalyaRajendran@users.noreply.github.com>
2023-07-20 20:12:34 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './style.css';
 
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-import Calculator from "./pages/Calculator";
-
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Calculator from './pages/Calculator';
+import Navbar from './pages/Navbar';
 
 function App() {
   return (
     <div className="App">
-      {/* <Signup></Signup> */}
-      {/* <Login></Login> */}
+      <Signup></Signup>
+      <Login></Login>
       <Calculator></Calculator>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../css/Navbar";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+          <li>
+            <a href="">Signup</a>
+          </li>
+          <li>
+            <a href="">Calculator</a>
+          </li>
+          <li>
+            <a href="">Idcard</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
