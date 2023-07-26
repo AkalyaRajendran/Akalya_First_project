a7d81e5797f65e7ec19a3faffb1ee0beac45b6f0
AkalyaRajendran <139786884+AkalyaRajendran@users.noreply.github.com>
2023-07-26 20:51:30 +0530

    calci worked

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -3,6 +3,20 @@ import '../css/Calculator';
 
 function Calculator() {
   const [value, setvalue] = React.useState('');
+  const clearfn = () => {
+    let valuearray = value.split('');
+    // if (valuearray.length > 0) {
+    //   valuearray.pop();
+    //   setvalue(valuearray.join(''));
+    // } else {
+    //   alert('Nothing to Delete');
+    // }
+
+    valuearray.length > 0
+      ? setvalue(valuearray.splice(0, valuearray.length - 1).join(''))
+      : alert('Nothing to Delete');
+  };
+
   return (
     <div className="Calculator_ak">
       <div className="calculator">
@@ -10,32 +24,32 @@ function Calculator() {
           <input type="text" placeholder="0" id="value" value={value} />
         </div>
         <div className="button">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>/</button>
+          <button onClick={() => setvalue(value + '7')}>7</button>
+          <button onClick={() => setvalue(value + '8')}>8</button>
+          <button onClick={() => setvalue(value + '9')}>9</button>
+          <button onClick={() => setvalue(value + '/')}>/</button>
         </div>
         <div className="button">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>*</button>
+          <button onClick={() => setvalue(value + '4')}>4</button>
+          <button onClick={() => setvalue(value + '5')}>5</button>
+          <button onClick={() => setvalue(value + '6')}>6</button>
+          <button onClick={() => setvalue(value + '*')}>*</button>
         </div>
         <div className="button">
           <button onClick={() => setvalue(value + '1')}>1</button>
           <button onClick={() => setvalue(value + '2')}>2</button>
           <button onClick={() => setvalue(value + '3')}>3</button>
-          <button>-</button>
+          <button onClick={() => setvalue(value + '3')}>-</button>
         </div>
         <div className="button">
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
-          <button>+</button>
+          <button onClick={() => setvalue(value + '0')}>0</button>
+          <button onClick={() => setvalue(value + '.')}>.</button>
+          <button onClick={() => setvalue(String(eval(value)))}>=</button>
+          <button onClick={() => setvalue(value + '+')}>+</button>
         </div>
         <div className="button">
-          <button>C</button>
-          <button>AC</button>
+          <button onClick={() => clearfn('')}>C</button>
+          <button onClick={() => setvalue('')}>AC</button>
         </div>
       </div>
     </div>
